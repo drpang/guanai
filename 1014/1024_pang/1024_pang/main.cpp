1b7c8c0ce4e26cae92b52633ad6e6abec745d9af
haobin.pang <haobin.pang@bugua.com>
2016-10-16 21:23:24 +0800

    创建工程

---
+++ 1014/1024_pang/1024_pang/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  1024_pang
+//
+//  Created by pang on 16/10/16.
+//  Copyright © 2016年 庞浩斌. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
